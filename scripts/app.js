83e9461d2a8102c0404d29d87e1f1ba6e123e932
Jahnvi <jahnvi18patel@gmail.com>
2021-02-20 19:02:14 +0000

    rows moving, before using map to select row array

--- scripts/app.js
+++ scripts/app.js
@@ -6,13 +6,15 @@ function init() {
   const width = 10
   const cellCount = width * width
   const cells = []
+  console.log(cells)
   //! Syringe
   const syringeClass = 'syringe'
   const syringeStartPosition = 95
   let syringeCurrentPosition = 95
   //! Covid19
   const covid19Class = 'covid19'
-  const covid19StartPosition = 0
+  const covid19StartPosition = 9
+
 
   //! Make a grid with syringe
   function createGrid(syringeStartPosition) {
@@ -31,6 +33,20 @@ function init() {
   function removeSyringe(position) {
     cells[position].classList.remove(syringeClass)
   }
+  //! Syringe movement
+  function handleKeyDown(event) {
+    const key = event.keyCode
+    removeSyringe(syringeCurrentPosition)
+    if (key === 39 && syringeCurrentPosition % width !== width - 1) {
+      syringeCurrentPosition++
+    } else if (key === 37 && syringeCurrentPosition % width !== 0) {
+      syringeCurrentPosition--
+    } else {
+      console.log('INVALID KEY')
+    }
+    addSyringe(syringeCurrentPosition)
+  }
+
   //! Add formation of virus
   function addCovid19() {
     for (let i = 4; i < 10; i++) {
@@ -51,22 +67,34 @@ function init() {
     }
   }
 
-  //! Syringe movement
-  function handleKeyDown(event) {
-    const key = event.keyCode
-    removeSyringe(syringeCurrentPosition)
-    if (key === 39 && syringeCurrentPosition % width !== width - 1) {
-      syringeCurrentPosition++
-    } else if (key === 37 && syringeCurrentPosition % width !== 0) {
-      syringeCurrentPosition--
-    } else {
-      console.log('INVALID KEY')
-    }
-    addSyringe(syringeCurrentPosition)
-  }
+  // function moveRow() {
+  //   setInterval(() => {
+  //     cells.forEach((cell, index) => {
+  const rowOne = cells.slice(0, 10)
+  console.log(rowOne)
+  // if (cell.className === covid19Class && 0 < index && index < 10) {
+  // if (cells[4] === 0) {
+  //     cell.classList.remove(covid19Class)
+  // rowOne[index + 10].classList.add(covid19Class)
+  //   } else {
+  //     cell.classList.remove(covid19Class)
+  //     cells[index - 1].classList.add(covid19Class)
+  //   }
+  // } else if (cell.className === covid19Class && 10 < index && index < 20) {
+  //   setTimeout(() => {
+  //     cell.classList.remove(covid19Class)
+  //     cells[index - 1].classList.add(covid19Class)
+  //   }, 400)
+  // }
+  //       })
+  //   }, 2000)
+  // }
+
+
   document.addEventListener('keydown', handleKeyDown)
   createGrid(syringeStartPosition)
   addCovid19()
+  // moveRow()
 }
 
 window.addEventListener('DOMContentLoaded', init)
